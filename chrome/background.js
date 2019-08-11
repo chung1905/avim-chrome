c29e3597bdbaa8ba192c18b4638506c7360427d7
chung <chung1905@gmail.com>
2019-08-11 17:48:29 +0700

    Map chrome api to firefox

--- chrome/background.js
+++ chrome/background.js
@@ -1,148 +1,165 @@
-(function(window){
-	var localStorage = window.localStorage;
-	
-	function setLocalStorageItem(key, value) {
-	  if (localStorage)
-		localStorage[key] = value;
-	}
+(function (window) {
+    // API Wrapper
+    var getAllTabsInWindow = function (windowId, callback) {
+        var tabs = browser.tabs.query({currentWindow: true});
+        tabs.then(callback);
+    };
+    var sendMessage2Tab = browser.tabs.sendMessage;
+    var browserAction = browser.browserAction;
+    var contextMenus = browser.menus;
+    var onMessage = browser.runtime.onMessage;
 
-	function getLocalStorageItem(key) {
-	  if (localStorage)
-		return localStorage.getItem(key);
+    // End API Wrapper
 
-	  return ;
-	}
+    var localStorage = window.localStorage;
 
-	function removeLocalStorageItem(key) {
-	  if (localStorage)
-		localStorage.removeItem(key);
-	}
+    function setLocalStorageItem(key, value) {
+        if (localStorage)
+            localStorage[key] = value;
+    }
 
-	function getPrefs(callback) {
-		if (!getLocalStorageItem('method')) {
-			init();
-		}
-		var prefs = {
-			'method': parseInt(getLocalStorageItem('method')),
-			'onOff': parseInt(getLocalStorageItem('onOff')),
-			'ckSpell': parseInt(getLocalStorageItem('ckSpell')),
-			'oldAccent': parseInt(getLocalStorageItem('oldAccent'))
-		};
-		
-		callback.call(this, prefs);
-	}
+    function getLocalStorageItem(key) {
+        if (localStorage)
+            return localStorage.getItem(key);
 
-	function turnAvim(callback) {
-		if (!getLocalStorageItem('method')) {
-			init();
-		}
+        return;
+    }
 
-		var onOff = getLocalStorageItem('onOff');
-		setLocalStorageItem('onOff', onOff=='1'?'0':'1');
+    function removeLocalStorageItem(key) {
+        if (localStorage)
+            localStorage.removeItem(key);
+    }
 
-		getPrefs(function(prefs){
-			updateAllTabs(prefs);
-			callback.call(this);
-		});
-	}
+    function getPrefs(callback) {
+        if (!getLocalStorageItem('method')) {
+            init();
+        }
+        var prefs = {
+            'method': parseInt(getLocalStorageItem('method')),
+            'onOff': parseInt(getLocalStorageItem('onOff')),
+            'ckSpell': parseInt(getLocalStorageItem('ckSpell')),
+            'oldAccent': parseInt(getLocalStorageItem('oldAccent'))
+        };
 
-	function updateAllTabs(prefs) {
-		chrome.tabs.getAllInWindow(null, function(tabs){
-			for (var i=0; i<tabs.length; i++) {
-				var tab = tabs[i];
-				chrome.tabs.sendMessage(tab.id, prefs);
-			}
-		});
-	
-		updateIcon(prefs);
-	}
+        callback.call(this, prefs);
+    }
 
-	function updateIcon(prefs) {
-		var txt = {};
-		var bg = {};
-	
-		if (prefs.onOff == 1) {
-			txt.text = "on";
-			bg.color = [0, 255, 0, 255];
-		} else {
-			txt.text = "off";
-			bg.color = [255, 0, 0, 255];
-		}
-	
-		chrome.browserAction.setBadgeText(txt);
-		chrome.browserAction.setBadgeBackgroundColor(bg);
-	}
+    function turnAvim(callback) {
+        if (!getLocalStorageItem('method')) {
+            init();
+        }
 
-	function savePrefs(request, callback) {
-		if (typeof request.method != 'undefined') {
-			setLocalStorageItem("method", request.method);
-		}
-		if (typeof request.onOff != 'undefined') {
-			setLocalStorageItem("onOff", request.onOff);
-		}
-		if (typeof request.ckSpell != 'undefined') {
-			setLocalStorageItem("ckSpell", request.ckSpell);
-		}
-		if (typeof request.oldAccent != 'undefined') {
-			setLocalStorageItem("oldAccent", request.oldAccent);
-		}
-	
-		getPrefs(function(prefs){
-			updateAllTabs(prefs);
-			callback.call(this);
-		});
-	}
+        var onOff = getLocalStorageItem('onOff');
+        setLocalStorageItem('onOff', onOff == '1' ? '0' : '1');
 
-	function processRequest(request, sender, sendResponse) {
-		if (request.get_prefs) {
-			getPrefs(sendResponse);
-			return;
-		}
-	
-		if (request.save_prefs) {
-			savePrefs(request, sendResponse);
-			return;
-		}
-	
-		if (request.turn_avim) {
-			turnAvim(sendResponse);
-			return;
-		}
-	}
-	
-	function genericOnClick() {
-		alert("demo");
-	}
-	
-	function createMenus() {
-		var parentId = chrome.contextMenus.create({"title" : "AVIM", "contexts" : ["selection"]});
-		var demo = chrome.contextMenus.create({"title" : "AVIM Demo", "contexts" : ["selection"], "parentId": parentId, "onclick": genericOnClick});
-	}
+        getPrefs(function (prefs) {
+            updateAllTabs(prefs);
+            callback.call(this);
+        });
+    }
 
-	function init() {
-		if (!getLocalStorageItem('method')) {
-			setLocalStorageItem('method', '0');
-		}
-	
-		if (!getLocalStorageItem('onOff')) {
-			setLocalStorageItem('onOff', '1');
-		}
-	
-		if (!getLocalStorageItem('ckSpell')) {
-			setLocalStorageItem('ckSpell', '1');
-		}
-	
-		if (!getLocalStorageItem('oldAccent')) {
-			setLocalStorageItem('oldAccent', '1');
-		}
-	
-		getPrefs(updateIcon);
-	
-		chrome.extension.onMessage.addListener(processRequest);
-		
-		//createMenus();
-	}
-	
-	init();
-	
+    function updateAllTabs(prefs) {
+        getAllTabsInWindow(null, function (tabs) {
+            for (var i = 0; i < tabs.length; i++) {
+                var tab = tabs[i];
+                sendMessage2Tab(tab.id, prefs);
+            }
+        });
+
+        updateIcon(prefs);
+    }
+
+    function updateIcon(prefs) {
+        var txt = {};
+        var bg = {};
+
+        if (prefs.onOff == 1) {
+            txt.text = "on";
+            bg.color = [0, 255, 0, 255];
+        } else {
+            txt.text = "off";
+            bg.color = [255, 0, 0, 255];
+        }
+
+        browserAction.setBadgeText(txt);
+        browserAction.setBadgeBackgroundColor(bg);
+    }
+
+    function savePrefs(request, callback) {
+        if (typeof request.method != 'undefined') {
+            setLocalStorageItem("method", request.method);
+        }
+        if (typeof request.onOff != 'undefined') {
+            setLocalStorageItem("onOff", request.onOff);
+        }
+        if (typeof request.ckSpell != 'undefined') {
+            setLocalStorageItem("ckSpell", request.ckSpell);
+        }
+        if (typeof request.oldAccent != 'undefined') {
+            setLocalStorageItem("oldAccent", request.oldAccent);
+        }
+
+        getPrefs(function (prefs) {
+            updateAllTabs(prefs);
+            callback.call(this);
+        });
+    }
+
+    function processRequest(request, sender, sendResponse) {
+        if (request.get_prefs) {
+            getPrefs(sendResponse);
+            return;
+        }
+
+        if (request.save_prefs) {
+            savePrefs(request, sendResponse);
+            return;
+        }
+
+        if (request.turn_avim) {
+            turnAvim(sendResponse);
+            return;
+        }
+    }
+
+    function genericOnClick() {
+        alert("demo");
+    }
+
+    function createMenus() {
+        var parentId = contextMenus.create({"title": "AVIM", "contexts": ["selection"]});
+        var demo = contextMenus.create({
+            "title": "AVIM Demo",
+            "contexts": ["selection"],
+            "parentId": parentId,
+            "onclick": genericOnClick
+        });
+    }
+
+    function init() {
+        if (!getLocalStorageItem('method')) {
+            setLocalStorageItem('method', '0');
+        }
+
+        if (!getLocalStorageItem('onOff')) {
+            setLocalStorageItem('onOff', '1');
+        }
+
+        if (!getLocalStorageItem('ckSpell')) {
+            setLocalStorageItem('ckSpell', '1');
+        }
+
+        if (!getLocalStorageItem('oldAccent')) {
+            setLocalStorageItem('oldAccent', '1');
+        }
+
+        getPrefs(updateIcon);
+
+        onMessage.addListener(processRequest);
+
+        //createMenus();
+    }
+
+    init();
+
 })(window);
